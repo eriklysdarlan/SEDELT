d6485b8517dbbcc8cfd44923cb637a2119eed4fb
prof-eriklys <eriklysdarlan@gmail.com>
2024-12-10 19:54:56 -0300

    adição de restrição as rotas administrativas

--- app/controller/admin/routes_admin.py
+++ app/controller/admin/routes_admin.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from flask_login import login_required
+from flask import Blueprint, render_template, request, redirect, url_for, abort
+from flask_login import login_required, current_user
 
 admin = Blueprint('admin', __name__)
 
 @login_required
-@admin.route('/admin')
+@admin.route('')
 def administrativControl():
+    if not current_user.is_admin:
+        abort(403)
     return render_template('admin/admin.html')
