d932bffef68020f90d53b929e233ee2b133c7a09
eriklys-linux <eriklysdarlan@gmail.com>
2024-12-25 18:39:55 -0300

    Recuperação de arquivo apgado script.js

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,12 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const path = window.location.pathname;
+    const buttons = document.querySelectorAll('.btn-admin-panel');
+    const adminContainer = document.getElementById('admin-container');
 
-function handleClick(event) {
-    
-    const buttons = document.querySelectorAll('.btn');
-    
     buttons.forEach(button => {
-        let value = button.getAttribute("aria-selected");
-        button.setAttribute("aria-selected", (value == "true") ? "false" : "true");
+        const tabPaneId = button.getAttribute('data-bs-target');
+        const tabPane = document.querySelector(tabPaneId);
+
+        if (path === button.getAttribute('data-url')) {
+            button.setAttribute('aria-selected', 'true');
+            button.classList.add('active');
+            if (tabPane) {
+                tabPane.classList.add('active', 'show');
+            }
+        } else {
+            button.setAttribute('aria-selected', 'false');
+            button.classList.remove('active');
+            if (tabPane) {
+                tabPane.classList.remove('active', 'show');
+            }
+        }
     });
-    
-    return window.location.href = event
+
+    // Remover a classe 'hidden' para mostrar o conteúdo
+    adminContainer.classList.remove('hidden');
+});
+function handleClick(url) {
+    window.location.href = url;
 }
